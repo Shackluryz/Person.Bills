ab71bba426d91ac3488c0cebfa963e222a8a72d5
Shackluryz <alem.shackuryz@gmail.com>
2021-04-08 16:45:59 -0300

    Floating Button Animation

--- PersonBills/app/src/main/java/com/aulas/personbills/DashBoardFragment.java
+++ PersonBills/app/src/main/java/com/aulas/personbills/DashBoardFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +24,21 @@ public class DashBoardFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    //Floating Button
+    private FloatingActionButton fab_main_btn;
+    private FloatingActionButton fab_income_btn;
+    private FloatingActionButton fab_expense_btn;
+
+    //Floating Button textview
+    private TextView fab_income_txt;
+    private TextView fab_expense_txt;
+
+    //boolean
+    private boolean isOpen=false;
+
+    //Animation
+    private Animation fadeOpen, fadeClose;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -56,9 +76,59 @@ public class DashBoardFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_board, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //Inflate the layout for this fragment
+        View myview=inflater.inflate(R.layout.fragment_dash_board, container, false);
+
+        //Connect floating button to layout
+        fab_main_btn=myview.findViewById(R.id.fb_main_plus_btn);
+        fab_income_btn=myview.findViewById(R.id.income_ft_btn);
+        fab_expense_btn=myview.findViewById(R.id.expense_ft_btn);
+
+        //Connect floating text
+        fab_income_txt=myview.findViewById(R.id.income_ft_text);
+        fab_expense_txt=myview.findViewById(R.id.expense_ft_text);
+
+        //Animation connect
+        fadeOpen= AnimationUtils.loadAnimation(getActivity(), R.anim.fade_open);
+        fadeClose= AnimationUtils.loadAnimation(getActivity(), R.anim.fade_close);
+
+        fab_main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(isOpen) {
+
+                    fab_income_btn.startAnimation(fadeClose);
+                    fab_expense_btn.startAnimation(fadeClose);
+                    fab_income_btn.setClickable(false);
+                    fab_expense_btn.setClickable(false);
+
+                    fab_income_txt.startAnimation(fadeClose);
+                    fab_expense_txt.startAnimation(fadeClose);
+                    fab_income_txt.setClickable(false);
+                    fab_expense_txt.setClickable(false);
+                    isOpen=false;
+
+                }else{
+
+                    fab_income_btn.startAnimation(fadeOpen);
+                    fab_expense_btn.startAnimation(fadeOpen);
+                    fab_income_btn.setClickable(true);
+                    fab_expense_btn.setClickable(true);
+
+                    fab_income_txt.startAnimation(fadeOpen);
+                    fab_expense_txt.startAnimation(fadeOpen);
+                    fab_income_txt.setClickable(true);
+                    fab_expense_txt.setClickable(true);
+                    isOpen=true;
+
+                }
+
+            }
+        });
+
+        return myview;
     }
 }
